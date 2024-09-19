e11128c3e179a199a1a2a25bb920baa146f527f8
Abdelahid Awessou <161177440+DVRTHVADER@users.noreply.github.com>
2024-09-19 14:00:01 -0400

    Add files via upload

---
+++ School database SQL/Test.sql
@@ -0,0 +1,10 @@
+
+-- Insertion d'un client dans la table client
+INSERT INTO client(client_ID, client_Name, client_Phone, client_Address)
+VALUES
+    (2, 'Abdelahmid Massou', '438-787-7143', '21 rue de Grand-Pre'),
+    (3, 'Abdulrashid Owissou', '438-787-7143', '21 rue de Grand-Pre'),
+    (4, 'Abdelsahmad Awou', '438-787-7143', '21 rue de Grand-Pre'),
+    (5, 'Abdelkadhim Belelou', '438-787-7143', '21 rue de Grand-Pre'),
+    (6, 'AbdelShazzam massetou', '438-787-7143', '21 rue de Grand-Pre');
+
